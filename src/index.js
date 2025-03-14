38b9b3c8f2102fdf213cb40ff3c3c41aac1896c4
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2025-03-14 20:45:14 +0530

    Add event listeners on tabs to load specific UI
    Added click event listeners on each navbar tab buttons to load specific
    UI contents for that tab. Initially only load the home UI on page load.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import loadHome from "./home";
 import loadMenu from "./menu";
 import loadContact from "./contact";
 
+const contentDiv = document.querySelector("#content");
+
+const homeTabButton = document.querySelector(".home");
+homeTabButton.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    loadHome();
+});
+
+const menuTabButton = document.querySelector(".menu");
+menuTabButton.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    loadMenu();
+});
+
+const contactTabButton = document.querySelector(".contact");
+contactTabButton.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    loadContact();
+});
+
 loadHome();
-loadMenu();
-loadContact();
